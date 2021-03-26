c609e45477b5e1e74e8659189033adff82168058
akhanf <alik@robarts.ca>
2021-03-26 15:51:56 -0400

    added generation of studyinstanceuid, pydicom

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['snakebids @ git+https://github.com/akhanf/snakebids.git@90b8a14bb4245e7a69d25745062848bef7a6182a' ]
+requirements = ['snakebids @ git+https://github.com/akhanf/snakebids.git@90b8a14bb4245e7a69d25745062848bef7a6182a',
+                    pydicom ]
 
 setup_requirements = [ ]
 
